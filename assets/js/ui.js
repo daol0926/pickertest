6a88449bbb0ce204f959814d7effb0adaa48bca2
daol0926 <dh.b0926@gmail.com>
2022-05-16 17:58:18 +0900

    css edit

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -527,9 +527,9 @@ function purposeSet(){
 	});
 }
 
-/* data picker */
+/* data picker - 생활비서 */
 function calPic() {
-	$('#datepicker').datepicker({
+	$('.data-month-selec').datepicker({
 		firstDay: 1,
 		monthNames: [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" ],
 		minDate: "-1m",
@@ -540,12 +540,39 @@ function calPic() {
 		//closeText: '닫기',
 		dayNamesMin: [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ]
 	});
+
 	$('.btn-show-calendar').click(function(){
 		$('.date-select').addClass('picker-full');
+		$(this).hide();
+		$('.dateSwiper').hide();
+		$('.btn-hide-calendar').show();
 		$(this).parents().find('body').addClass('scroll-y-hidden');
 	});
 	$('.btn-hide-calendar').click(function(){
 		$('.date-select').removeClass('picker-full');
+		$(this).hide();
+		$('.btn-show-calendar').show();
+		$('.dateSwiper').show();
 		$(this).parents().find('body').removeClass('scroll-y-hidden');
+	});
+}
+
+/* data picker - 자산관리 이력정보 */
+function dataCalPic() {
+	$('.date-start').datepicker({
+		firstDay: 1,
+		monthNames: [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" ],
+		//showButtonPanel: true,
+		//showAnim: "slideDown",
+		//closeText: '닫기',
+		dayNamesMin: [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ]
+	});
+	$('.date-end').datepicker({
+		firstDay: 1,
+		monthNames: [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" ],
+		//showButtonPanel: true,
+		//showAnim: "slideDown",
+		//closeText: '닫기',
+		dayNamesMin: [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ]
 	});
 }
